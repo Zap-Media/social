0cbf6acbe440590a6a4873d3151b65098c7ce98d
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-23 19:23:45 +0530

    home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Zap Social",
-  description: "Zap media's Social media platform",
+    title: "Zap Social",
+    description: "Zap media's Social media platform",
 };
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
-  return (
-    <html lang="en "data-theme="light">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en "data-theme="light">
+            <body>{children}</body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 
-
 export default function Home() {
-  return (
-    <main>
-      <center>
-        <h1>Zap Social</h1>
-      </center>
-    </main>
-  );
+    return (
+        <div className="main">
+            <div className="text-center">
+                <i><h1 className='title'>Zap Social</h1></i>
+                <div className='btn-grid'>
+                    <button className="btn btn-info">Continue with Google</button>
+                    <button className="btn btn-primary">Continue with Discord</button>
+                </div>
+            </div>
+        </div>
+    );
 }
